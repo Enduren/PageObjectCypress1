a9b9156133d3061fce48e3eff8fcb66458cea9fb
Dexter Dwayne Tennison <83023757+Enduren@users.noreply.github.com>
2024-09-25 21:45:00 -0500

    create new test

---
+++ cypress/e2e/Automation/hiddenElements.cy.js
@@ -0,0 +1,11 @@
+describe('Hidden Elements', function () {
+    // test case
+    it('Scenario 1', function (){
+       // launch URL
+       cy.visit("https://www.amazon.com/");
+       // show hidden element with invoke
+       cy.get('#nav-flyout-ya-signin').invoke('show');
+       //click hidden element
+       cy.contains('Sign').click();
+    });
+ });
